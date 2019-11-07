8371a26c79f6bdbd951b310640779b1341f312c0
kaenes <knsart@knsart.pl>
2019-11-07 20:22:29 +0100

    2 files modified

--- Table.js
+++ Table.js
@@ -2,41 +2,40 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Table extends React.Component {
+
   
   render(){
-    let myArray = ["John","Mick","Jane","Lola"];
-    let myName = myArray.map( (name) =>  <li>{name}</li>);
+  const { characterData, removeCharacter } = this.props;
+  console.log(this.props);
+    const TableHead = () => {
+        return(
+             <thead>
+        <tr> 
+          <th>Name </th>
+          <th>Job </th>
+        </tr>
+      </thead>
+        );
+    };
+    const TableBody = props => {
+      const rows = props.characterData.map((row,index) => {
+        return(
+        <tr key={index}>
+        <td> {row.name} </td>
+        <td> {row.job} </td>
+        <td> <button onClick={ () => props.removeCharacter(index)}>Delete </button> </td>
+        </tr>
+        );
+     
+      });
+      return <tbody>{rows}</tbody>
+      
+    }
     return(
       <div>
-      <ul>
-        {myName}  
-      </ul>
-
       <table className="table table-hover bg-color-red">
-      <thead>
-      <tr>
-      <th>Name</th>
-      <th>Job</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-            <td className="text-danger">Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-      </tbody>
+      <TableHead />
+      <TableBody characterData = {characterData} removeCharacter = { removeCharacter}/>
       </table>
       </div>
     );
--- index.js
+++ index.js
@@ -9,11 +9,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+       characters: [
+          {name: 'Charlie', job: 'Janitor'},
+          {name: 'Mack', job: 'Bouncer'},
+          {name: 'Dee', job: 'Aspring actress'},
+          {name: 'Dennis', job: 'Bartender'},
+        ],
     };
   }
+removeCharacter = index => {
+        const { characters } = this.state;
 
+        this.setState({
+          characters: characters.filter((character,i)=>{
+            return i !== index;
+          })
+        });
+      }
+    
   render() {
+      const { characters } = this.state;
+      
+   
     return (
       <div className="">
       <h1  className = "">
@@ -21,7 +38,7 @@ class App extends Component {
         <p className="">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not on
         </p>
-        <Table />
+        <Table characterData = { characters } removeCharacter = {this.removeCharacter}/>
       </div>
     );
   }
