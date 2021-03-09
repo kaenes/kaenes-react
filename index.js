c3f54a83ef0d97f2a01818b341c6080fe79ed02b
kaenes <knsart@knsart.pl>
2021-03-09 20:36:20 +0100

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import Table from './Table.js';
-import './style.css';
-
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import Table from "./Table.js";
+import "./style.css";
+import TestComponent from "./testComponent.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-       characters: [
-          {name: 'Charlie', job: 'Janitor'},
-          {name: 'Mack', job: 'Bouncer'},
-          {name: 'Dee', job: 'Aspring actress'},
-          {name: 'Dennis', job: 'Bartender'},
-        ],
+      characters: [
+        { name: "Charlie", job: "Janitor" },
+        { name: "Mack", job: "Bouncer" },
+        { name: "Dee", job: "Aspring actress" },
+        { name: "Dennis", job: "Bartender" }
+      ]
     };
   }
-removeCharacter = index => {
-        const { characters } = this.state;
+  removeCharacter = index => {
+    const { characters } = this.state;
 
-        this.setState({
-          characters: characters.filter((character,i)=>{
-            return i !== index;
-          })
-        });
-      }
-    
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
+
   render() {
-      const { characters } = this.state;
-      
-   
+    const { characters } = this.state;
+
     return (
       <div className="">
-      <h1  className = "">
-        <Hello name={this.state.name} /> </h1>
+        <h1 className="">
+          <TestComponent />
+          <Hello name={this.state.name} />{" "}
+        </h1>
         <p className="">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not on
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not on
         </p>
-        <Table characterData = { characters } removeCharacter = {this.removeCharacter}/>
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
---
+++ testComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComponent = props => {
+  return (
+    <div>
+      <h1>dsfsdfsdfdsf</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
